576a5933ce4a7aed9e973e9093e1aa48b5891666
Alpha <AlphaOmegaxUK@outlook.com>
2026-09-12 16:50:20 +0100

    Update tmp-ls-music_convert.ps1

--- Tools/convertors/audio/tmp-ls-music_convert.ps1
+++ Tools/convertors/audio/tmp-ls-music_convert.ps1
@@ -2,6 +2,18 @@
 #   InsideEARTH - Earth 2150 Audio Convertor v1.0
 # =====================================================================
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Self-elevation check
+$isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if (-not $isAdmin) {
+    Write-Host "Requesting Administrator privileges..." -ForegroundColor Yellow
+    # Relaunches the current script with elevated privileges and keeps the window open
+    Start-Process powershell -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
+    Exit
+}
+
 clear
 
 $host.ui.RawUI.WindowTitle = "InsideEARTH - Earth 2150 Video Convertor"
