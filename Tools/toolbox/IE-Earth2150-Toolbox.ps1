67f98d2be18453b937e853a10238f2608ea717ef
Alpha <AlphaOmegaxUK@outlook.com>
2026-09-13 20:19:43 +0100

    Update IE-Earth2150-Toolbox.ps1

--- Tools/toolbox/IE-Earth2150-Toolbox.ps1
+++ Tools/toolbox/IE-Earth2150-Toolbox.ps1
@@ -33,7 +33,7 @@ while ($true) {
     Write-Host "   [3] Levels Downloader"
     Write-Host "   [4] Media Converter (TMP/LS ONLY)"
     Write-Host "   [5] Registry Editor"
-    Write-Host ""
+    Write-Host
     Write-Host "   [6] Exit" -ForegroundColor Red
     Write-Host
 
